c98927448f867b73ad2a26543897cfb23851bfa6
George Valdes <gv2186@gmail.com>
2018-02-25 03:36:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,19 @@ function logWhisper(string)
 {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string)
+{
+  var hi = string
+  if (hi.toLowerCase() === hi) {
+     return "I can't hear you!"
+  } else if (hi.toUpperCase() === hi){
+     return "YES INDEED!"
+  } else if (hi === "I love you, Grandma."){
+     return "I love you, too."
+  }
+}
+
+
+
+
