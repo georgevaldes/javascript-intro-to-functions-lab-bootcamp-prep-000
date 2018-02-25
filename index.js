57897787c27427ebd4467b7210e0a0ac14b31e88
George Valdes <gv2186@gmail.com>
2018-02-25 02:47:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,26 +8,3 @@ function logShout(string)
   console.log(string.toUpperCase())
 }
 
-function sayHiToGrandma(string)
-{
-  var hello = string
-  return "I can't hear you!"
-  return "YES INDEED!"
-  return "I love you, too."
-  if (string === "hungry") {
-  console.log("Refilling food bowl.")
- 
-} else if (dog === "thirsty") {
-  console.log("Refilling water bowl.")
- 
-} else if (dog === "playful") {
-  console.log("Playing tug-of-war.")
- 
-} else if (dog === "cuddly") {
-  console.log("Snuggling.")
- 
-} else { 
-  console.log("Reading newspaper.")
-}
-}
-
