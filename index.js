e2bed045115a42c220902b5201aeb17d333df659
George Valdes <gv2186@gmail.com>
2018-02-25 02:44:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string)
+{
+  return string.toUpperCase()
+}
+
+function logShout(string)
+{
+  console.log(string.toUpperCase())
+}
+
+function sayHiToGrandma(string)
+{
+  return "I can't hear you!"
+  return "YES INDEED!"
+  return "I love you, too."
+}
